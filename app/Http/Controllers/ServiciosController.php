162ba7e5663d0d7ba676fcd4f3f1d281f237d6a2
BJBP <jaison.03042000@gmail.com>
2024-07-11 20:48:03 -0500

    a

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Servicio;
+use App\Http\Requests\CreateServicioRequest;
 
 class ServiciosController extends Controller
 {
@@ -24,7 +25,9 @@ class ServiciosController extends Controller
         return view('create');
     }
 
-    public function store(){
+    public function store(CreateServicioRequest $request){
+        
+        Servicio::create($request->validated());
         // Recogemos Las variables
         $camposv = request()->validate([
             'titulo' => 'required',
